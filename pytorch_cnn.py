40fe8aae0f9a3f462ae0422ad6c9b80ccc5a5749
Hannes <ga74bux@mytum.de>
2020-02-21 10:59:11 +0100

    Changed to Binary Classification Network arquitechture

--- pytorch_cnn.py
+++ pytorch_cnn.py
@@ -11,9 +11,10 @@ import torchvision.transforms as transforms
 from torch.utils.data.sampler import WeightedRandomSampler
 from polyaxon_client.tracking import Experiment, get_data_paths, get_outputs_path
 
+# TF compat export: https://pytorch.org/tutorials/advanced/super_resolution_with_onnxruntime.html
+
 # PARAMETERS
 
-BATCH_SIZE = 32
 IMG_HEIGHT  = 272
 IMG_WIDTH   = 258
 train_on_gpu = torch.cuda.is_available()
@@ -26,10 +27,12 @@ if cluster:
     train_data_path = data_paths['data1'] + "/HHase_Robotic_RL/Sacrum_Classification/testing/"
     val_data_path = data_paths['data1'] + "/HHase_Robotic_RL/Sacrum_Classification/validation/"
     output_path = get_outputs_path()
+    BATCH_SIZE = 128
 else:
-    train_data_path = "/Data/training/"
-    val_data_path = "/Data/validation/"
-    output_path = "/logs/"
+    train_data_path = "./Data/training/"
+    val_data_path = "./Data/validation/"
+    output_path = "./logs/"
+    BATCH_SIZE = 16
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
@@ -58,7 +61,7 @@ samples_weights = class_weights[train_data.targets]
 
 sampler = torch.utils.data.sampler.WeightedRandomSampler(samples_weights, num_samples=len(train_data), replacement=True)
 
-train_data_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=128, sampler=sampler, pin_memory=train_on_gpu, num_workers=4)
+train_data_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, sampler=sampler, pin_memory=train_on_gpu, num_workers=4)
 
 # LOAD VALIDATION DATA
 
@@ -74,7 +77,7 @@ class_weights = 1./torch.tensor(class_sample_counts, dtype=torch.float)
 samples_weights = class_weights[validation_data.targets]
 validation_sampler = torch.utils.data.sampler.WeightedRandomSampler(samples_weights, num_samples=len(train_data), replacement=True)
 
-validation_data_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=128, sampler=sampler, pin_memory=False)#, num_workers=4)
+validation_data_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, sampler=sampler, pin_memory=train_on_gpu)#, num_workers=4)
 
 # INITIALIZE MODEL
 
@@ -89,17 +92,31 @@ class ConvNet(nn.Module):
             nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
         self.drop_out = nn.Dropout()
-        self.fc1 = nn.Linear(int(IMG_HEIGHT/4) * int(IMG_WIDTH/4) * 64, 1000)
-        self.fc2 = nn.Linear(1000, 10)
+        self.fc1 = nn.Linear(int(IMG_HEIGHT/16) * int(IMG_WIDTH/16) * 256, 1000)
+        self.fc2 = nn.Linear(1000, 300)
+        self.drop_out2 = nn.Dropout()
+        self.fc3 = nn.Linear(300, 1)
 
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
         out = out.reshape(out.size(0), -1)
         out = self.drop_out(out)
         out = self.fc1(out)
+        out = self.drop_out2(out)
         out = self.fc2(out)
+        out = self.fc3(out)
         return out
 
 model = ConvNet()
@@ -108,10 +125,10 @@ print(model)
 if train_on_gpu:
     model.cuda()
 
-criterion = torch.nn.CrossEntropyLoss()
+criterion = torch.nn.BCEWithLogitsLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr = 0.003, momentum= 0.9)
 
-n_epochs = 5  # you may increase this number to train a final model
+n_epochs = 50  # you may increase this number to train a final model
 valid_loss_min = np.Inf  # track change in validation loss
 
 for epoch in range(1, n_epochs + 1):
@@ -131,6 +148,7 @@ for epoch in range(1, n_epochs + 1):
             data, target = data.cuda(), target.cuda()
         optimizer.zero_grad()
         output = model(data)
+        target = target.unsqueeze(1).type_as(output)
         loss = criterion(output, target)
         loss.backward()
         optimizer.step()
@@ -144,6 +162,7 @@ for epoch in range(1, n_epochs + 1):
         if train_on_gpu:
             data, target = data.cuda(), target.cuda()
         output = model(data)
+        target = target.unsqueeze(1).type_as(output)
         loss = criterion(output, target)
         valid_loss += loss.item() * data.size(0)
 
